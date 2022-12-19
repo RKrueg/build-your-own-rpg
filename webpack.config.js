82cb15dc24fce72c48fd535271210ac91d0b95a4
RKrueg <thegckid@gmail.com>
2022-12-18 16:48:19 -0800

    add lint, babel and webpackages

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');  
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {                
+    contentBase: './dist'      
+  }, 
+  devtool: 'eval-source-map',
+  plugins: [
+    new ESLintPlugin({
+      option1: value
+    }),
+    new CleanWebpackPlugin({
+      verbose: true
+    }), 
+    new HtmlWebpackPlugin({
+      title: 'build-your-own-rpg',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
