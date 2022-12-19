3540ee924b418f57776fef86985a3acf8a606a42
RKrueg <thegckid@gmail.com>
2022-12-18 16:37:36 -0800

    add the beginning of our monster classes
    Co-authored-by: Ariel-Doyle <Ariel-Doyle@users.noreply.github.com>

---
+++ src/monster.js
@@ -0,0 +1,27 @@
+// export function diceRoll() {
+//   return Math.floor((Math.random() * 20) + 1);
+// }
+
+// export class Monster {
+//   constructor (strength, health) {
+  // this.strength = strength;
+  // this.health = health;
+//   }
+// }
+
+// export class MiniBoss {
+//   constructor (strength, health) {
+  // this.strength = strength;
+  // this.health = health;
+//   }
+// }
+
+// export class Boss {
+//   constructor (strength, health) {
+  // this.strength = strength;
+  // this.health = health;
+//   }
+// }
+
+
+
